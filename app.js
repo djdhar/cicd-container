1c2909fa710199fab94f491592c38c89123021d4
djdhar <dibyajyotidhar@gmail.com>
2021-09-30 11:11:04 +0530

    changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World! DJ"));
+app.get("/", (req, res) => res.send("Hello World DJ! Now fully Automated!!!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
